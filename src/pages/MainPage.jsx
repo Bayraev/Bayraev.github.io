569d0588001866aec659f31cacb7effcccfa4e61
Alialisk <99403653+Aliasik@users.noreply.github.com>
2023-06-02 15:11:33 +0300

    test

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,6 +6,7 @@ import RightBar from '../components/RightBar';
 const MainPage = () => {
   return (
     <div className={styles.wrapper}>
+      test
       <LeftBar />
       <RightBar />
     </div>
